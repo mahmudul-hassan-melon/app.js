98f634a0e229047c9c0cf4487e4410265dd16737
mahmudul-hassan-melon <mahmudulhassanmelon@gmail.com>
2023-02-24 15:15:03 +0600

    salary

---
+++ salary.js
@@ -0,0 +1,10 @@
+let basicSalary =18000;
+
+if(basicSalary >= 10000 ){
+    console.log(10000 + 500 + ( basicSalary * .25) +1000 -( basicSalary * .15 ) );
+ } else if(basicSalary >= 50000 ){
+    console.log(50000 + 2000 + ( basicSalary * .25) +1000 -( basicSalary * .15 ) );
+}
+
+// else if(marks >=33 && marks < 39 ) {
+//      console.log("I  got D Grgde in the exam" );
