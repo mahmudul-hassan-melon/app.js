bba7a98e5d91a70d2a4847740557d6a8004b2d60
mahmudul-hassan-melon <mahmudulhassanmelon@gmail.com>
2023-02-24 12:09:20 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let marks = 80;
+
+
+// if statement
+
+if(marks < 33){
+    console.log("I failed in the exam" );
+}else if(marks >=33 && marks < 39 ) {
+     console.log("I  got D Grgde in the exam" );
+} else if(marks >=40 && marks < 49 ) {
+    console.log("I  got C Grgde in the exam" );
+} else if(marks >=50 && marks < 59 ) {
+    console.log("I  got B Grgde in the exam" );
+} else if(marks >=60 && marks < 69 ) {
+    console.log("I  got A - Grgde in the exam" );
+} else if(marks >=70 && marks < 79 ) {
+    console.log("I  got A Grgde in the exam" );
+} else if(marks >=80 && marks < 100 ) {
+    console.log("I  got A+ Grgde in the exam" );
+} 
+
+
+
